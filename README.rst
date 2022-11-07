66b91b00b1a69af088043861a3d95bc8edcc02c6
mathurinm <mathurin.massias@gmail.com>
2022-11-07 16:15:45 +0100

    Add objective and README

---
+++ README.rst
@@ -0,0 +1,48 @@
+Benchmark repository for SLOPE
+==============================
+
+|Build Status| |Python 3.6+|
+
+This repository is based on the work of Johan Larsson, Quentin Klopfenstein and Jonas Wallin at https://github.com/Klopfe/benchmark_slope.
+
+Benchopt is a package to simplify and make more transparent and
+reproducible the comparisons of optimization algorithms.
+Regression with the Sorted L-One Penalized Estimation (SLOPE) estimator which consists in solving the following program:
+
+$$ \\min_{\\beta} \\, \\tfrac{1}{2n} \\Vert y - X\\beta \\Vert^2_2 + J(\\beta, \\lambda) $$
+
+where
+
+$$ J(\\beta, \\lambda) = \\sum_{j=1}^p \\lambda_j \| \\beta_{(j)}\| $$
+
+with $\\lambda_1 \\geq \\lambda_2 \\geq ... \\geq \\lambda_p$ and $\|\\beta_{(1)}\| \\geq \|\\beta_{(2)}\| \\geq ... \\geq \|\\beta_{(p)}\|$.
+
+We note $n$ (or n_samples) the number of samples and $p$ (or n_features) the number of features.
+We also have that $X\\in \\mathbb{R}^{n\\times p}$ and $y\\in \\mathbb{R}^n$.
+
+
+
+Install
+--------
+
+This benchmark can be run using the following commands:
+
+.. code-block::
+
+   $ pip install -U benchopt
+   $ git clone https://github.com/benchopt/benchmark_slope
+   $ benchopt run ./benchmark_slope
+
+Apart from the problem, options can be passed to `benchopt run`, to restrict the benchmarks to some solvers or datasets, e.g.:
+
+.. code-block::
+
+	$ benchopt run ./benchmark_slope -s cd -d simulated --max-runs 10 --n-repetitions 5
+
+
+Use `benchopt run -h` for more details about these options, or visit https://benchopt.github.io/cli.html.
+
+.. |Build Status| image:: https://github.com/benchopt/benchmark_mcp/workflows/Tests/badge.svg
+   :target: https://github.com/benchopt/benchmark_mcp/actions
+.. |Python 3.6+| image:: https://img.shields.io/badge/python-3.6%2B-blue
+   :target: https://www.python.org/downloads/release/python-360/
---
+++ objective.py
@@ -0,0 +1,72 @@
+# Author: Quentin Klopfenstein
+#         Jonas Wallin
+#         Johan Larsson
+
+from benchopt import BaseObjective, safe_import_context
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+    from numpy.linalg import norm
+    from scipy import stats
+
+
+class Objective(BaseObjective):
+    name = "SLOPE"
+    parameters = {
+        "reg": [0.5, 0.1, 0.02],
+        "q": [0.2, 0.1, 0.05],
+        "fit_intercept": [True, False],
+    }
+
+    def __init__(self, reg, q, fit_intercept):
+        self.q = q
+        self.reg = reg
+        self.fit_intercept = fit_intercept
+
+    def set_data(self, X, y):
+        self.X, self.y = X, y
+        self.n_samples, self.n_features = self.X.shape
+        self.alphas = self._get_lambda_seq()
+
+    def compute(self, res):
+        intercept, beta = res[0], res[1:]
+
+        X, y = self.X, self.y
+        n_samples = X.shape[0]
+        # compute residuals
+        diff = y - X @ beta - intercept
+
+        # compute primal
+        p_obj = 1.0 / (2 * n_samples) * diff @ diff + np.sum(
+            self.alphas * np.sort(np.abs(beta))[::-1]
+        )
+
+        # compute dual
+        theta = diff
+        theta /= max(1, self._dual_norm_slope(theta, self.alphas))
+        d_obj = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / (2 * n_samples)
+
+        return dict(value=p_obj, duality_gap=p_obj - d_obj)
+
+    def to_dict(self):
+        return dict(
+            X=self.X, y=self.y, alphas=self.alphas, fit_intercept=self.fit_intercept
+        )
+
+    def _dual_norm_slope(self, theta, alphas):
+        Xtheta = np.sort(np.abs(self.X.T @ theta))[::-1]
+        taus = 1 / np.cumsum(alphas)
+        return np.max(np.cumsum(Xtheta) * taus)
+
+    def _get_lambda_seq(self):
+        randnorm = stats.norm(loc=0, scale=1)
+        q = self.q
+        alphas_seq = randnorm.ppf(
+            1 - np.arange(1, self.X.shape[1] + 1) * q / (2 * self.X.shape[1])
+        )
+
+        alpha_max = self._dual_norm_slope(
+            (self.y - self.fit_intercept * np.mean(self.y)) / len(self.y), alphas_seq
+        )
+
+        return alpha_max * alphas_seq * self.reg
