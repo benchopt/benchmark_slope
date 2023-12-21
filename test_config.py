4d1132d19885781212b2e37a12cbe53efe213fb9
Johan Larsson <johanlarsson@outlook.com>
2023-12-21 16:36:49 +0100

    tests: add test_config to avoid rslope on mac

---
+++ test_config.py
@@ -0,0 +1,7 @@
+import sys
+import pytest
+
+
+def check_test_solver_install(solver_class):
+    if solver_class.name.lower() == "rslope" and sys.platform == "darwin":
+        pytest.xfail("Dependencies for R packages fail to install on MacOS.")
