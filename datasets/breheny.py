26ed4f25c2b2b4ef8e1cc75fd7352a388a1913d2
Johan Larsson <jolars@posteo.com>
2025-06-06 19:49:42 +0200

    fix: use correct cache dir for breheny data

--- datasets/breheny.py
+++ datasets/breheny.py
@@ -15,7 +15,7 @@ with safe_import_context() as import_ctx:
 
 
 def fetch_breheny(dataset: str):
-    base_dir = appdirs.user_cache_dir("benchmark_lasso_path")
+    base_dir = appdirs.user_cache_dir("benchmark_slope")
 
     path = os.path.join(base_dir, dataset + ".rds")
 
